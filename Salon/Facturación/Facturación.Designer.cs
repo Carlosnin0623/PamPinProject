b5ae1a7e42142f117b488d84436d78251b505936
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2025-02-14 09:04:39 -0400

    Sistema Pamela

---
+++ Salon/Facturación/Facturación.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Salon.Facturación
+{
+    partial class Facturación
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DgvDatosEmpleados = new DrakeUI.Framework.DrakeUIDataGridView();
+            this.Edit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID_Empleado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cedula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sexo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDatosEmpleados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvDatosEmpleados
+            // 
+            this.DgvDatosEmpleados.AllowUserToAddRows = false;
+            this.DgvDatosEmpleados.AllowUserToDeleteRows = false;
+            this.DgvDatosEmpleados.AllowUserToResizeColumns = false;
+            this.DgvDatosEmpleados.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.DgvDatosEmpleados.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.DgvDatosEmpleados.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DgvDatosEmpleados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.DgvDatosEmpleados.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.DgvDatosEmpleados.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.DgvDatosEmpleados.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DgvDatosEmpleados.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.RaisedHorizontal;
+            this.DgvDatosEmpleados.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Silver;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Silver;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvDatosEmpleados.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.DgvDatosEmpleados.ColumnHeadersHeight = 32;
+            this.DgvDatosEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.DgvDatosEmpleados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Edit,
+            this.ID_Empleado,
+            this.Nombre,
+            this.Apellido,
+            this.Cedula,
+            this.Sexo,
+            this.Estado});
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DgvDatosEmpleados.DefaultCellStyle = dataGridViewCellStyle4;
+            this.DgvDatosEmpleados.EnableHeadersVisualStyles = false;
+            this.DgvDatosEmpleados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.DgvDatosEmpleados.GridColor = System.Drawing.Color.Silver;
+            this.DgvDatosEmpleados.Location = new System.Drawing.Point(12, 166);
+            this.DgvDatosEmpleados.Name = "DgvDatosEmpleados";
+            this.DgvDatosEmpleados.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.DgvDatosEmpleados.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvDatosEmpleados.RowHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.DgvDatosEmpleados.RowHeadersVisible = false;
+            this.DgvDatosEmpleados.RowHeadersWidth = 25;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DgvDatosEmpleados.RowsDefaultCellStyle = dataGridViewCellStyle6;
+            this.DgvDatosEmpleados.RowTemplate.Height = 29;
+            this.DgvDatosEmpleados.SelectedIndex = -1;
+            this.DgvDatosEmpleados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DgvDatosEmpleados.ShowRect = false;
+            this.DgvDatosEmpleados.Size = new System.Drawing.Size(926, 354);
+            this.DgvDatosEmpleados.StripeOddColor = System.Drawing.Color.White;
+            this.DgvDatosEmpleados.Style = DrakeUI.Framework.UIStyle.Custom;
+            this.DgvDatosEmpleados.StyleCustomMode = true;
+            this.DgvDatosEmpleados.TabIndex = 57;
+            // 
+            // Edit
+            // 
+            this.Edit.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Edit.HeaderText = "";
+            this.Edit.Name = "Edit";
+            this.Edit.Width = 55;
+            // 
+            // ID_Empleado
+            // 
+            this.ID_Empleado.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.ID_Empleado.DataPropertyName = "ID_Empleado";
+            this.ID_Empleado.HeaderText = "ID";
+            this.ID_Empleado.Name = "ID_Empleado";
+            this.ID_Empleado.Width = 95;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Nombre.HeaderText = "Fact No.";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 130;
+            // 
+            // Apellido
+            // 
+            this.Apellido.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Apellido.DataPropertyName = "Apellido";
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.Name = "Apellido";
+            this.Apellido.Width = 130;
+            // 
+            // Cedula
+            // 
+            this.Cedula.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Cedula.DataPropertyName = "Cedula";
+            this.Cedula.HeaderText = "Cédula";
+            this.Cedula.Name = "Cedula";
+            this.Cedula.Width = 130;
+            // 
+            // Sexo
+            // 
+            this.Sexo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Sexo.DataPropertyName = "Sexo";
+            this.Sexo.HeaderText = "Sexo";
+            this.Sexo.Name = "Sexo";
+            this.Sexo.Width = 120;
+            // 
+            // Estado
+            // 
+            this.Estado.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Estado.DataPropertyName = "Estado";
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Estado.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Estado.HeaderText = "Estado";
+            this.Estado.Name = "Estado";
+            // 
+            // Facturación
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 545);
+            this.Controls.Add(this.DgvDatosEmpleados);
+            this.Name = "Facturación";
+            this.Text = "Facturación";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDatosEmpleados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DrakeUI.Framework.DrakeUIDataGridView DgvDatosEmpleados;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Edit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID_Empleado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cedula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sexo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Estado;
+    }
+}
---
+++ Salon/Login/Login.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Salon
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.drakeUITextBox1 = new DrakeUI.Framework.DrakeUITextBox();
+            this.drakeUITextBox2 = new DrakeUI.Framework.DrakeUITextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Closelogin = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Closelogin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(82)))), ((int)(((byte)(134)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(748, 445);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(82)))), ((int)(((byte)(134)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(529, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Login";
+            // 
+            // drakeUITextBox1
+            // 
+            this.drakeUITextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(82)))), ((int)(((byte)(134)))));
+            this.drakeUITextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.drakeUITextBox1.FillColor = System.Drawing.SystemColors.Window;
+            this.drakeUITextBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drakeUITextBox1.Location = new System.Drawing.Point(444, 163);
+            this.drakeUITextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.drakeUITextBox1.Maximum = 2147483647D;
+            this.drakeUITextBox1.Minimum = -2147483648D;
+            this.drakeUITextBox1.Name = "drakeUITextBox1";
+            this.drakeUITextBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.drakeUITextBox1.Radius = 0;
+            this.drakeUITextBox1.RectColor = System.Drawing.Color.Silver;
+            this.drakeUITextBox1.RectDisableColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.drakeUITextBox1.Size = new System.Drawing.Size(249, 29);
+            this.drakeUITextBox1.Style = DrakeUI.Framework.UIStyle.Custom;
+            this.drakeUITextBox1.TabIndex = 7;
+            this.drakeUITextBox1.Text = "Usuario";
+            // 
+            // drakeUITextBox2
+            // 
+            this.drakeUITextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(82)))), ((int)(((byte)(134)))));
+            this.drakeUITextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.drakeUITextBox2.FillColor = System.Drawing.Color.White;
+            this.drakeUITextBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drakeUITextBox2.Location = new System.Drawing.Point(444, 222);
+            this.drakeUITextBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.drakeUITextBox2.Maximum = 2147483647D;
+            this.drakeUITextBox2.Minimum = -2147483648D;
+            this.drakeUITextBox2.Name = "drakeUITextBox2";
+            this.drakeUITextBox2.Padding = new System.Windows.Forms.Padding(5);
+            this.drakeUITextBox2.Radius = 0;
+            this.drakeUITextBox2.RectColor = System.Drawing.Color.Silver;
+            this.drakeUITextBox2.RectDisableColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(242)))), ((int)(((byte)(253)))));
+            this.drakeUITextBox2.Size = new System.Drawing.Size(249, 29);
+            this.drakeUITextBox2.Style = DrakeUI.Framework.UIStyle.Custom;
+            this.drakeUITextBox2.TabIndex = 8;
+            this.drakeUITextBox2.Text = "Contraseña";
+            // 
+            // Closelogin
+            // 
+            this.Closelogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Closelogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(82)))), ((int)(((byte)(134)))));
+            this.Closelogin.Image = global::Salon.Properties.Resources.icons8_multiply_23;
+            this.Closelogin.Location = new System.Drawing.Point(714, 5);
+            this.Closelogin.Name = "Closelogin";
+            this.Closelogin.Size = new System.Drawing.Size(28, 24);
+            this.Closelogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Closelogin.TabIndex = 143;
+            this.Closelogin.TabStop = false;
+            this.Closelogin.Click += new System.EventHandler(this.Closelogin_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Salon.Properties.Resources.front_view_woman_getting_her_manicure_done_at_the_salon_scaled_1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(354, 446);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(534, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 144;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 445);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Closelogin);
+            this.Controls.Add(this.drakeUITextBox2);
+            this.Controls.Add(this.drakeUITextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Closelogin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private DrakeUI.Framework.DrakeUITextBox drakeUITextBox1;
+        private DrakeUI.Framework.DrakeUITextBox drakeUITextBox2;
+        private System.Windows.Forms.PictureBox Closelogin;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
